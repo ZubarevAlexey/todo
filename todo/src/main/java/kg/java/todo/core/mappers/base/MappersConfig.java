94dc151163797f83e8168612dc69ee12f03bb6a8
User <stparchkg@gmail.com>
2023-11-29 15:23:21 +0600

    -add CategoryMapper

--- todo/src/main/java/kg/java/todo/core/mappers/base/MappersConfig.java
+++ todo/src/main/java/kg/java/todo/core/mappers/base/MappersConfig.java
@@ -1,14 +1,19 @@
 package kg.java.todo.core.mappers.base;
 
+import kg.java.todo.core.mappers.CategoryMapper;
 import kg.java.todo.core.mappers.UserMapper;
-import lombok.Builder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class MappersConfig {
     @Bean
-    public UserMapper buildUserMapper(){
+    public UserMapper buildUserMapper() {
         return new UserMapper();
+    }
+
+    @Bean
+    public CategoryMapper buildCategoryMapper() {
+        return new CategoryMapper();
     }
 }
