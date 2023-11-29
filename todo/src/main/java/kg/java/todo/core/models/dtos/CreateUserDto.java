b3d471ec40db5f2e973f19e74ce055644d783570
User <stparchkg@gmail.com>
2023-11-29 14:40:52 +0600

    -add dtos for UserEntity

---
+++ todo/src/main/java/kg/java/todo/core/models/dtos/CreateUserDto.java
@@ -0,0 +1,17 @@
+package kg.java.todo.core.models.dtos;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class CreateUserDto {
+    private String firstName;
+    private String lastName;
+    private String phoneNumber;
+
+}
---
+++ todo/src/main/java/kg/java/todo/core/models/dtos/UserDto.java
@@ -0,0 +1,18 @@
+package kg.java.todo.core.models.dtos;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class UserDto {
+    private Long id;
+    private String firstName;
+    private String lastName;
+    private String phoneNumber;
+
+}
