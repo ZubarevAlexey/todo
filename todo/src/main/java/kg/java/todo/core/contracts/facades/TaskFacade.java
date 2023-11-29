7e97547d25546c10b4fc89a3ad4332ae28807fd4
User <stparchkg@gmail.com>
2023-11-29 20:18:13 +0600

    add TaskFacade

---
+++ todo/src/main/java/kg/java/todo/core/contracts/facades/TaskFacade.java
@@ -0,0 +1,13 @@
+package kg.java.todo.core.contracts.facades;
+
+import kg.java.todo.core.exceptions.EntityDuplicateException;
+import kg.java.todo.core.exceptions.EntityNotFoundException;
+import kg.java.todo.core.models.dtos.task.*;
+import org.springframework.http.HttpStatus;
+
+public interface TaskFacade {
+    TaskDto add(CreateTaskDto model) throws EntityDuplicateException, EntityNotFoundException;
+    TaskDto update(UpdateTaskDto model) throws EntityNotFoundException;
+    HttpStatus delete(DeleteTaskDto model) throws EntityNotFoundException;
+    TaskDto findById(FindByIdTaskDto model) throws EntityNotFoundException;
+}
